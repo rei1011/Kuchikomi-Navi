66b63b4673cccbaa7f211ac398f998b96bd00833
rei1011 <r.mizobuchi3@gmail.com>
2024-05-03 02:48:27 +0000

    add comment api definition

--- backend/spec/requests/api/comments_spec.rb
+++ backend/spec/requests/api/comments_spec.rb
@@ -6,9 +6,20 @@ RSpec.describe 'api/comments', type: :request do
   path '/comments' do
     get 'Get Comments' do
       tags 'Comments'
-      consumes 'application/json'
-
-      response '200', 'get comments' do
+      produces 'application/json'
+      response '200', 'comment found' do
+        schema type: :array,
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   store_name: { type: :string },
+                   prostutite_name: { type: :string },
+                   comment: { type: :string },
+                   publication_date: { type: :date }
+                 },
+                 required: %i[id store_name prostutite_name comment publication_date]
+               }
         run_test!
       end
     end
