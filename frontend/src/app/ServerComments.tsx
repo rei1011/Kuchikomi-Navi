6fec42327e1b5cc5cd0e2de5944196f5bc022855
rei1011 <r.mizobuchi3@gmail.com>
2024-05-04 07:33:56 +0000

    sample server fetch

---
+++ frontend/src/app/ServerComments.tsx
@@ -0,0 +1,18 @@
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export async function ServerComments() {
+  const data = await getData();
+
+  return <main>{JSON.stringify(data)}</main>;
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Comments } from "@/Comments";
+import { Comments } from "./Comments";
+import { ServerComments } from "./ServerComments";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Comments />
+      <ServerComments />
     </main>
   );
 }
