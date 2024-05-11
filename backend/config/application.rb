38c5a55ce19162a5c431d16f29cb066382035bc2
rei1011 <r.mizobuchi3@gmail.com>
2024-05-11 14:53:35 +0000

    remove env

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -16,15 +16,6 @@ module Backend
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
-    puts '## DATABASE_PASSWORD ##'
-    puts ENV['DATABASE_PASSWORD']
-    puts '## DATABASE_PASSWORD_TEST ##'
-    puts ENV['DATABASE_PASSWORD_TEST']
-    puts '## RAILS_MASTER_KEY ##'
-    puts ENV['RAILS_MASTER_KEY']
-    puts '## RAILS_MASTER_KEY_TEST ##'
-    puts ENV['RAILS_MASTER_KEY_TEST']
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
