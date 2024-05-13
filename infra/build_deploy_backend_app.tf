ca347dd10bcee7853fac4284aaace7a8f5ab9003
rei1011 <r.mizobuchi3@gmail.com>
2024-05-13 03:49:24 +0000

    set allow origins

--- infra/build_deploy_backend_app.tf
+++ infra/build_deploy_backend_app.tf
@@ -88,5 +88,6 @@ resource "google_cloudbuild_trigger" "backend_app_trigger" {
     _ARTIFACT_REPOSITORY_IMAGE_NAME = "${var.region}-docker.pkg.dev/${var.project}/${var.backend_app_name}/${var.backend_app_name}"
     _DATABASE_PASSWORD_KEY          = var.database_password_key
     _INSTANCE_NAME                  = var.database_instance_name
+    _ALLOW_ORIGINS                  = "https://frontend-app-uuwuit3kva-de.a.run.app"
   }
 }
