d4d57128b541c5d5fd1ba71b5ebc5d2e3c0c0c13
rei1011 <r.mizobuchi3@gmail.com>
2024-05-15 09:45:20 +0000

    sidekiq setting comment out

--- backend/config/initializers/sidekiq.rb
+++ backend/config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}/#{ENV["REDIS_DB_NUMBER"]}" }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}/#{ENV["REDIS_DB_NUMBER"]}" }
+# end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}/#{ENV["REDIS_DB_NUMBER"]}" }
-end
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: "redis://#{ENV["REDIS_HOST"]}:#{ENV["REDIS_PORT"]}/#{ENV["REDIS_DB_NUMBER"]}" }
+# end
