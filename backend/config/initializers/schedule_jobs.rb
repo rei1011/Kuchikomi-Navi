5b6ebc0f83ba42e4e6ec37d9550d30ee92fa9cf8
rei1011 <r.mizobuchi3@gmail.com>
2024-05-16 01:10:19 +0000

    add example job

--- backend/config/initializers/schedule_jobs.rb
+++ backend/config/initializers/schedule_jobs.rb
@@ -1,13 +1,13 @@
 Rails.application.config.after_initialize do
-  # if ENV["RUN_JOB"] == "true" then
-  #   comments = Comment.all;
-  #   p "show commets"
-  #   p comments
+  if ENV["RUN_JOB"] == "true" then
+    comments = Comment.all;
+    p "show commets"
+    p comments
 
-  #   Thread.new do
-  #     sleep 1 # ジョブ登録が完了するのを待つために少し待機
-  #     Rails.logger.info "Job registration completed. Shutting down application."
-  #     exit(0)
-  #   end
-  # end
+    Thread.new do
+      sleep 1 # ジョブ登録が完了するのを待つために少し待機
+      Rails.logger.info "Job registration completed. Shutting down application."
+      exit(0)
+    end
+  end
 end
