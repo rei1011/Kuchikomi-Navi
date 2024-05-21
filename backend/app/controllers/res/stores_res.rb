b27b6c50842fd1804167d8ff642585d55b566e96
rei1011 <r.mizobuchi3@gmail.com>
2024-05-21 08:12:22 +0000

    Revert "fix StoresRes module"
    This reverts commit 64e2549216e075a49ed3693f821ffdd9b28b5c6f.

--- backend/app/controllers/res/stores_res.rb
+++ backend/app/controllers/res/stores_res.rb
@@ -1,20 +1,18 @@
 # frozen_string_literal: true
 
-module Res
-  # 店舗の一覧を返却するための型
-  class StoresRes
-    def initialize(stores)
-      @list = []
-      stores.all.each do |store|
-        @list.push({ id: store[:id],
-                     store_name: store[:store_name],
-                     address: {
-                       prefecture: store[:prefecture],
-                       municipality: store[:municipality]
-                     },
-                     home_page: store[:home_page],
-                     store_image: store[:store_image] })
-      end
+# 店舗の一覧を返却するための型
+class StoresRes
+  def initialize(stores)
+    @list = []
+    stores.all.each do |store|
+      @list.push({ id: store[:id],
+                   store_name: store[:store_name],
+                   address: {
+                     prefecture: store[:prefecture],
+                     municipality: store[:municipality]
+                   },
+                   home_page: store[:home_page],
+                   store_image: store[:store_image] })
     end
   end
 end
