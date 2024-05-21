4eded7731f7f581683de32e20988f920fcac1195
rei1011 <r.mizobuchi3@gmail.com>
2024-05-21 03:52:37 +0000

    incorporating RHF into the search bar

--- frontend/src/component/SearchField.tsx
+++ frontend/src/component/SearchField.tsx
@@ -1,21 +1,49 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import SearchIcon from "@mui/icons-material/Search";
 import { InputAdornment, TextField } from "@mui/material";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 
 /**
- * 検索欄
- * @returns
+ * バリデーション定義
  */
-export function SearchField() {
+const schema = z.object({
+  keyword: z.string(),
+});
+type SchemaType = z.infer<typeof schema>;
+
+/**
+ * props
+ */
+type SearchFieldType = {
+  submit: (data: SchemaType) => void;
+};
+
+/**
+ * 検索バー
+ */
+export function SearchField(props: SearchFieldType) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SchemaType>({ resolver: zodResolver(schema) });
+  const { submit } = props;
+  const onSubmit: SubmitHandler<SchemaType> = (data) => submit(data);
+
   return (
-    <TextField
-      variant="outlined"
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon />
-          </InputAdornment>
-        ),
-      }}
-    />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        {...register("keyword")}
+        variant="outlined"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+    </form>
   );
 }
