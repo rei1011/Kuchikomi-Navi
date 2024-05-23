56d84fc927fc69f1ca4fb83ba915a29726a4987a
rei1011 <r.mizobuchi3@gmail.com>
2024-05-23 00:42:42 +0000

    fix path for fujoho spec

--- backend/spec/services/fujoho/fujoho_service_spec.rb
+++ backend/spec/services/fujoho/fujoho_service_spec.rb
@@ -5,19 +5,19 @@ require 'rails_helper'
 RSpec.describe 'get store from fujoho page' do
   it do
     # 店舗一覧のmock
-    raw_shop_list_file = File.new('/usr/src/app/backend/spec/services/shop_list_response.txt')
+    raw_shop_list_file = File.new('/usr/src/app/backend/spec/services/fujoho/shop_list_response.txt')
     stub_request(:get, %r{https://fujoho\.jp/index\.php\?(.*&)?p=shop_list(&.*)?$})
       .to_return(status: 200, body: raw_shop_list_file, headers: { content_type: 'text/html' })
 
     # 店舗一覧のmock（3ページ目に表示される店舗数が0件と仮定）
-    raw_empty_shop_list_file = File.new('/usr/src/app/backend/spec/services/empty_shop_list_response copy.txt')
+    raw_empty_shop_list_file = File.new('/usr/src/app/backend/spec/services/fujoho/empty_shop_list_response copy.txt')
     stub_request(:get, 'https://fujoho.jp/index.php?p=shop_list&w=M%E6%80%A7%E6%84%9F&b=2')
       .to_return(status: 200,
                  body: raw_empty_shop_list_file,
                  headers: { content_type: 'text/html' })
 
     # 店舗詳細のmock
-    raw_shop_detail_file = File.new('/usr/src/app/backend/spec/services/shop_detail_response.txt')
+    raw_shop_detail_file = File.new('/usr/src/app/backend/spec/services/fujoho/shop_detail_response.txt')
     stub_request(:get, %r{https://fujoho\.jp/index\.php\?id=\d+&p=shop})
       .to_return(status: 200, body: raw_shop_detail_file, headers: { content_type: 'text/html' })
 
