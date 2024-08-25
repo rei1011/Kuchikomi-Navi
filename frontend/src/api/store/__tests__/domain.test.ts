eeee9f78f0e8c1085ecaedb03a601a23518db1ae
rei1011 <r.mizobuchi3@gmail.com>
2024-08-25 20:46:04 +0900

    commit for test

--- frontend/src/api/store/__tests__/domain.test.ts
+++ frontend/src/api/store/__tests__/domain.test.ts
@@ -3,6 +3,7 @@ import { convertStoreData } from "../domain";
 
 describe("store domain", () => {
   test("convert store data", () => {
+    console.log("hoge")
     const result = convertStoreData();
     expect(result).toStrictEqual({});
   });
