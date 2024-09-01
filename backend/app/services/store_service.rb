173bd7084d777d645eeb7b20e05412e5ffd9f1fd
rei1011 <r.mizobuchi3@gmail.com>
2024-09-01 13:23:53 +0000

    不要なコードを削除

--- backend/app/services/store_service.rb
+++ backend/app/services/store_service.rb
@@ -5,12 +5,6 @@ class StoreService
   def self.find(search_word)
     return StoreRepository.find_all if search_word.empty
 
-    puts '全ての店舗を取得します' if search_word == '全て'
-
-    StoreRepository.find_by_keyword(search_word.value)
-  end
-
-  def self.find_test
     StoreRepository.find_by_keyword(search_word.value)
   end
 end
