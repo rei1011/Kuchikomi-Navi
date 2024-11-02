6d47baf6b5721f04b6110ff2ebdc8cb99f21cf72
rei1011 <r.mizobuchi3@gmail.com>
2024-11-02 10:32:18 +0000

    店舗入力欄のheaderを作成

--- frontend/src/app/report/input/AddStoreArea.tsx
+++ frontend/src/app/report/input/AddStoreArea.tsx
@@ -8,14 +8,17 @@ export const AddStoreArea = () => {
   const { setSelectedIndex, selectedStore } = useReportContext();
   return useMemo(
     () => (
-      <div className="flex flex-col gap-8">
-        <Link href="/report/search" onClick={() => setSelectedIndex(0)}>
-          <AddStore selectedStore={selectedStore[0]?.label} />
-        </Link>
-        <Link href="/report/search" onClick={() => setSelectedIndex(1)}>
-          <AddStore selectedStore={selectedStore[1]?.label} />
-        </Link>
-      </div>
+      <section className="flex flex-col gap-4">
+        <h1 className="text-xl">店舗を選択</h1>
+        <div className="flex flex-col gap-8">
+          <Link href="/report/search" onClick={() => setSelectedIndex(0)}>
+            <AddStore selectedStore={selectedStore[0]?.label} />
+          </Link>
+          <Link href="/report/search" onClick={() => setSelectedIndex(1)}>
+            <AddStore selectedStore={selectedStore[1]?.label} />
+          </Link>
+        </div>
+      </section>
     ),
     [selectedStore, setSelectedIndex]
   );
