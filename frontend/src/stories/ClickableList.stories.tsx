c43bcf469b51923d2fb53a39ed2315daff37b123
rei1011 <r.mizobuchi3@gmail.com>
2024-11-06 14:58:28 +0000

    StoryBookの修正

--- frontend/src/stories/ClickableList.stories.tsx
+++ frontend/src/stories/ClickableList.stories.tsx
@@ -14,9 +14,9 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     items: [
-      { title: "Basic認証のエラー対応", roomId: "uuid1" },
-      { title: "美味しいカレーの作り方", roomId: "uuid2" },
-      { title: "公的医療保険の例", roomId: "uuid3" },
+      { name: "Basic認証のエラー対応", id: 1 },
+      { name: "美味しいカレーの作り方", id: 2 },
+      { name: "公的医療保険の例", id: 3 },
     ],
   },
 };
