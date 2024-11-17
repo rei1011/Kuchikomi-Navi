f46526b23f30c652c2521a27b3c7916be1d74d95
rei1011 <r.mizobuchi3@gmail.com>
2024-11-17 23:25:00 +0900

    Revert "inputページを削除"
    This reverts commit ed615930d07f8b17de93fb11eb0c074376e6f69e.

---
+++ frontend/src/app/report/rooms/[id]/input/page.tsx
@@ -0,0 +1,25 @@
+import { AppBarWithMenu } from "@/component/AppBarWithMenu";
+import { Main } from "@/component/Main";
+import { AddStoreSection } from "./AddStoreSection";
+import { ChatSection } from "./ChatSection";
+import { SendMessageSection } from "./SendMessageSection";
+
+export default async function ReportInput({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  return (
+    <Main paddingBottom={false}>
+      <div className="flex flex-col gap-4 w-full h-full">
+        <AppBarWithMenu />
+        <div className="flex flex-col gap-8 px-4 w-full justify-between overscroll-y-auto">
+          <AddStoreSection id={id} />
+          <ChatSection />
+        </div>
+      </div>
+      <SendMessageSection />
+    </Main>
+  );
+}
