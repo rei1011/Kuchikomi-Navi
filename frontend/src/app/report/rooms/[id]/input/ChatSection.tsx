ed615930d07f8b17de93fb11eb0c074376e6f69e
rei1011 <r.mizobuchi3@gmail.com>
2024-11-17 14:20:33 +0000

    inputページを削除

--- frontend/src/app/report/rooms/[id]/input/ChatSection.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useReportContext } from "@/app/report-context";
-
-export const ChatSection = () => {
-  const { report } = useReportContext();
-  return <div className="w-4/5">{report}</div>;
-};
