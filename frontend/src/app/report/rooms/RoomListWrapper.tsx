dbd2a7ca0abfe04ab0fcb90fa684d1c6dd94ca21
rei1011 <r.mizobuchi3@gmail.com>
2024-11-18 14:03:47 +0000

    チャットルームを取得するAPIを一時的にdisable

--- frontend/src/app/report/rooms/RoomListWrapper.tsx
+++ frontend/src/app/report/rooms/RoomListWrapper.tsx
@@ -1,11 +1,9 @@
-import { findAllRooms } from "@/api/report/rooms/api";
-import { RoomList } from "./RoomList";
-
 export const RoomListWrapper = async () => {
-  const rooms = await findAllRooms();
-  if (!rooms) {
-    // TODO: EmptyViewの作成
-    return <>Empty View</>;
-  }
-  return <RoomList rooms={rooms.list} />;
+  // const rooms = await findAllRooms();
+  // if (!rooms) {
+  //   // TODO: EmptyViewの作成
+  //   return <>Empty View</>;
+  // }
+  return <>Empty View</>;
+  // return <RoomList rooms={rooms.list} />;
 };
