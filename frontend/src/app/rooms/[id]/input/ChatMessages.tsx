f340d285b6762987e72c05d2c4e3fc0ce1adeb39
rei1011 <r.mizobuchi3@gmail.com>
2024-12-07 09:23:02 +0000

    メッセージを受信すると画面下部に自動でスクロールする

--- frontend/src/app/rooms/[id]/input/ChatMessages.tsx
+++ frontend/src/app/rooms/[id]/input/ChatMessages.tsx
@@ -2,6 +2,7 @@
 
 import { Messages } from "@/api/report/type";
 import { ChatMessage } from "@/component/ChatMessage";
+import { useEffect, useRef } from "react";
 import { useReportContext } from "../report-context";
 
 type Props = {
@@ -11,6 +12,15 @@ type Props = {
 export const ChatMessages = (props: Props) => {
   const { messages } = props;
   const { sseMessage } = useReportContext();
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+  }, [sseMessage]);
+
   return (
     <div className="w-full">
       <div className="flex flex-col gap-4">
@@ -22,6 +32,7 @@ export const ChatMessages = (props: Props) => {
           />
         ))}
         {sseMessage && <ChatMessage $role="assistant" message={sseMessage} />}
+        <div ref={messagesEndRef} />
       </div>
     </div>
   );
