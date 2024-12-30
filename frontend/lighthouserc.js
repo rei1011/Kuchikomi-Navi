a52755dc53122edfe1e220c759bf9a5c528c74cf
rei1011 <r.mizobuchi3@gmail.com>
2024-12-31 00:38:25 +0900

    テスト対象のURLを追加

--- frontend/lighthouserc.js
+++ frontend/lighthouserc.js
@@ -2,9 +2,14 @@ module.exports = {
   ci: {
     collect: {
       settings: {
-        extraHeaders: JSON.stringify({Authorization: "Basic YnVjaGlyZWk6a3VjaGlrb21pMDExNA=="}),
+        extraHeaders: JSON.stringify({
+          Authorization: "Basic YnVjaGlyZWk6a3VjaGlrb21pMDExNA==",
+        }),
       },
-      url: ["https://frontend-app-uuwuit3kva-de.a.run.app/"],
+      url: [
+        "https://frontend-app-uuwuit3kva-de.a.run.app/",
+        "https://frontend-app-uuwuit3kva-de.a.run.app/rooms",
+      ],
     },
     upload: {
       target: "temporary-public-storage",
