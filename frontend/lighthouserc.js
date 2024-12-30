b775a0228a840cdb7f939288d5b8df955857f4c3
rei1011 <r.mizobuchi3@gmail.com>
2024-12-31 00:29:55 +0900

    extraheadersを設定

--- frontend/lighthouserc.js
+++ frontend/lighthouserc.js
@@ -1,6 +1,9 @@
 module.exports = {
   ci: {
     collect: {
+      settings: {
+        extraHeaders: JSON.stringify({Authorization: "Basic YnVjaGlyZWk6a3VjaGlrb21pMDExNA=="}),
+      },
       url: ["https://frontend-app-uuwuit3kva-de.a.run.app/"],
     },
     upload: {
