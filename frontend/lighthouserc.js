50eacf39dfadf579baadc2b40d766bd82e8539ae
rei1011 <r.mizobuchi3@gmail.com>
2024-12-31 11:32:28 +0900

    header情報を環境変数から取得するようにした

--- frontend/lighthouserc.js
+++ frontend/lighthouserc.js
@@ -1,11 +1,23 @@
+const BASIC_AUTH_USER = process.env.BASIC_AUTH_USER;
+const BASIC_AUTH_PASSWORD = process.env.BASIC_AUTH_PASSWORD;
+
+// 環境変数が正しく設定されているか確認
+if (!BASIC_AUTH_USER || !BASIC_AUTH_PASSWORD) {
+  throw new Error(
+    "BASIC_AUTH_USERまたはBASIC_AUTH_PASSWORDが設定されていません。"
+  );
+}
+
+const basicAuthHeader = `Basic ${Buffer.from(`${BASIC_AUTH_USER}:${BASIC_AUTH_PASSWORD}`).toString("base64")}`;
+
 module.exports = {
   ci: {
     collect: {
-      // settings: {
-      //   extraHeaders: JSON.stringify({
-      //     Authorization: "Basic YnVjaGlyZWk6a3VjaGlrb21pMDExNA==",
-      //   }),
-      // },
+      settings: {
+        extraHeaders: JSON.stringify({
+          Authorization: basicAuthHeader,
+        }),
+      },
       url: [
         "https://frontend-app-uuwuit3kva-de.a.run.app/",
         "https://frontend-app-uuwuit3kva-de.a.run.app/rooms",
