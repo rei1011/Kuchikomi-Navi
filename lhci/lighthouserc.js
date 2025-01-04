00e5f1e783cb9c7b7f76e98a93fde03aa591ca8c
rei1011 <r.mizobuchi3@gmail.com>
2025-01-04 15:59:32 +0900

    configファイルを一つにまとめた

--- lhci/lighthouserc.js
+++ lhci/lighthouserc.js
@@ -26,5 +26,23 @@ module.exports = {
         password: BASIC_AUTH_PASSWORD,
       },
     },
+    server: {
+      port: 9001,
+      storage: {
+        storageMethod: "sql",
+        sqlDialect: "sqlite",
+        sqlDatabasePath: "/data/lhci.db",
+      },
+      basicAuth: {
+        username: BASIC_AUTH_USER,
+        password: BASIC_AUTH_PASSWORD,
+      },
+      deleteOldBuildsCron: [
+        {
+          maxAgeInDays: 1,
+          schedule: "0 */1 * * *",
+        },
+      ],
+    },
   },
 };
