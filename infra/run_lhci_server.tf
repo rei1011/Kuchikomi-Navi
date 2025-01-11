a901aec26d66d1330bd92f8295ede3ac07ace74e
rei1011 <r.mizobuchi3@gmail.com>
2025-01-11 02:53:41 +0000

    費用を削減するためにCPUを常に割り当てないようにした

--- infra/run_lhci_server.tf
+++ infra/run_lhci_server.tf
@@ -1,25 +1,17 @@
 # lhci serverを実行するためのCloud Runの設定
-# cronで履歴を削除するために最小インスタンス数を1にし、CPUを常に割り当てている
 resource "google_cloud_run_v2_service" "lhci_server" {
   name     = var.lhci_server_name
   location = var.region
 
   template {
     scaling {
-      min_instance_count = 1
+      min_instance_count = 0
       max_instance_count = 1
     }
     containers {
       image = "${var.region}-docker.pkg.dev/${var.project}/${var.lhci_server_name}/${var.lhci_image_name}:latest"
       ports {
         container_port = 9001
-      }
-      resources {
-        cpu_idle = false
-        limits = {
-          cpu    = "1"
-          memory = "512Mi"
-        }
       }
     }
   }
