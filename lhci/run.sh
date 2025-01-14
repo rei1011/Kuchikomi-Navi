955ba2dd3c02740f7ce6805f648b5e376bac310e
rei1011 <r.mizobuchi3@gmail.com>
2025-01-14 23:39:17 +0900

    Dockerfile and run.sh updates: simplify installation steps and enhance environment variable handling

--- lhci/run.sh
+++ lhci/run.sh
@@ -5,7 +5,7 @@ set -e
 rm -f /data/lhci.db
 
 # Restore the database file
-litestream restore -if-replica-exists -config /etc/litestream.yml /data/lhci.db
+litestream restore -if-replica-exists /data/lhci.db
 
 # Replicate the database file and start the LHCI server
-litestream replicate -exec "pnpm start" -config /etc/litestream.yml
+litestream replicate -exec "pnpm start"
